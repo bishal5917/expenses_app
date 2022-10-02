ee90964412cf2d5b45f965d9a29f3832581d8d8d
bishal5917 <poudelb172@gmail.com>
2022-10-02 11:22:18 +0545

    transactions class ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -13,14 +15,41 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 'i1',
+        title: "Lenovo Legion Laptop",
+        amount: 1055,
+        date: DateTime.now()),
+    Transaction(
+        id: 'i2', title: "Naviforce Watch", amount: 29, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Expenses Tracker',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Expenses Tracker'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Card(
+              color: Color.fromARGB(255, 178, 182, 182),
+              child: Container(
+                child: Text("Chart"),
+                width: 100,
+              ),
+              elevation: 5,
+            ),
+            Card(
+              child: Container(
+                child: Text("List of TXs"),
+              ),
+              elevation: 5,
+            )
+          ],
         ),
       ),
     );
