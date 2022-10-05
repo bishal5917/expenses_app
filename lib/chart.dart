07857563f80e78ce8974931342be92c788b73308
bishal5917 <poudelb172@gmail.com>
2022-10-05 21:13:53 +0545

    chart widget initialized

--- lib/chart.dart
+++ lib/chart.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_app/chartbar.dart';
 import 'package:expenses_app/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -38,6 +39,7 @@ class Chart extends StatelessWidget {
         ...(groupedTransactionValues).map((data) {
           return Text(data['day']);
         }),
+        // ChartBar()
       ]),
     );
   }
